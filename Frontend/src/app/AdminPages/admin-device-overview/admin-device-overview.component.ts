ad0d6034cdb3c1be63ba9d5474b8b69189e84652
MarcusIversen <91528331+MarcusIversen@users.noreply.github.com>
2022-11-22 12:58:29 +0100

    Frontend fix

---
+++ Frontend/src/app/AdminPages/admin-device-overview/admin-device-overview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-device-overview',
+  templateUrl: './admin-device-overview.component.html',
+  styleUrls: ['./admin-device-overview.component.scss']
+})
+export class AdminDeviceOverviewComponent {
+
+}
---
+++ Frontend/src/app/UserPages/device-registration/device-registration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-device-registration',
+  templateUrl: './device-registration.component.html',
+  styleUrls: ['./device-registration.component.scss']
+})
+export class DeviceRegistrationComponent {
+
+}
